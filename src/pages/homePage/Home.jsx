e6937eb793e59b7e38c25644a8e9572c2d5f5adb
geygrill <vera.van.veenhuizen@gmail.com>
2026-04-18 02:37:07 +0200

    home page intro and login and register buttons

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,9 +1,22 @@
 import './Home.css'
+import {Link} from "react-router-dom";
 
 function Home() {
     return (
         <>
+            <header className="header outer-content-container">
+                <div className="intro-inner-container">
+                    <h1>Welkom bij PokéQuest!</h1>
+                    <p className="intro-text">
+                        Test je Pokémon-kennis, bouw je droomteam en ontdek al je favoriete Pokémon!
+                    </p>
 
+                    <div className="intro-buttons">
+                        <Link to="/register" className="btn-intro-primary">Maak een account</Link>
+                        <Link to="/login" className="btn-intro-secondary">Inloggen</Link>
+                    </div>
+                </div>
+            </header>
         </>
     )
 }

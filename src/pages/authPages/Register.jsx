7e94628c2061e78e83159c6db8b84960106fe2cd
geygrill <vera.van.veenhuizen@gmail.com>
2026-04-21 11:28:50 +0200

    main wrapper around register

--- src/pages/authPages/Register.jsx
+++ src/pages/authPages/Register.jsx
@@ -38,7 +38,7 @@ function Register() {
     }
 
     return (
-        <div className="outer-content-container auth-page">
+        <main className="outer-content-container auth-page">
             <div className="auth-card">
                 <Pokeball size="medium" />
                 <h1>Account aanmaken</h1>
@@ -108,7 +108,7 @@ function Register() {
                     Al een account? <Link to="/login">Inloggen</Link>
                 </p>
             </div>
-        </div>
+        </main>
     )
 }
 
